d7f44f7cbae36535cc46c009ac40f54716308511
Skaronea <skaroneae@bodhiudevl01.udev.six3>
2018-05-22 08:27:25 -0600

    Moved gradle files to root directory

---
+++ src/main/java/hello/HelloWorld.java
@@ -0,0 +1,10 @@
+package hello;
+
+
+public class HelloWorld{
+    public static void main(String[] args) {
+        Greeter greet = new Greeter();
+
+        System.out.println(greet.sayHello());
+    }
+}
